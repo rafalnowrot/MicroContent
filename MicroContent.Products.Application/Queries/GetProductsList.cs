a751bb937bd402e57a3ae75586bd10302dc599bf
rnowrot <rafal.nowrot97@gmail.com>
2023-08-11 14:06:58 +0200

    c

--- MicroContent.Products.Application/Queries/GetProductsList.cs
+++ MicroContent.Products.Application/Queries/GetProductsList.cs
@@ -1,10 +1,12 @@
 ﻿using MediatR;
 using MicroContent.Products.Application.Dto;
 using MicroContent.Products.Domain.Interface;
+using MicroContent.Products.Domain.Types;
+using MicroContent.Products.Domain.Value_Objects;
 
 namespace MicroContent.Products.Application.Queries;
 
-public record GetProductsList() : IRequest<List<ProductDto>>;
+public record GetProductsList() : IRequest<IEnumerable<ProductDto>>;
 
 public class GetProductsListHandler : IRequestHandler<GetProductsList, IEnumerable<ProductDto>>
 {
@@ -17,8 +19,15 @@ public class GetProductsListHandler : IRequestHandler<GetProductsList, IEnumerab
 
     public async Task<IEnumerable<ProductDto>> Handle(GetProductsList request, CancellationToken cancellationToken)
     {
-        var response = (await _productsService.GetAll()).ToList();
+        var response = (await _productsService.GetAll())
+            .Select(x=>new ProductDto
+            {
+                Id = new ProductId(x.Id), 
+                Name = x.Name,
+                Price = x.Price,
+                Status = new ProductType(x.Status)
+            }).AsEnumerable();
 
-        return  Enumerable.Empty<ProductDto>();
+        return response;
     }
 }
